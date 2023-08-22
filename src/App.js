fc8c8d719dfcfe0876a8566d7be2313de456ed75
ayush9522 <ayush9522>
2023-08-22 17:10:15 +0530

    ui

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Header from './Component/Header'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+      </div>
+    )
+  }
+}
+
